d6c574690e6e9ad51e682c1637335d62795a5dbe
jatin19k <44009818+jatin19k@users.noreply.github.com>
2020-10-17 14:19:37 -0400

    Add files via upload

---
+++ GitHub/Hydroxychloroquine/Cubes/scp.sh
@@ -0,0 +1,5 @@
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/hydroxychloroquine/HOMO_440_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/hydroxychloroquine/final/Cubes
+
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/hydroxychloroquine/SOMO_441_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/hydroxychloroquine/final/Cubes
+
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/hydroxychloroquine/LUMO_442_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/hydroxychloroquine/final/Cubes
