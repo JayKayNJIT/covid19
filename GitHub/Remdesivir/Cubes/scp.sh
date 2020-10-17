534c767b28d37a6c5644ced9aff81ba74edc4e4f
jatin19k <44009818+jatin19k@users.noreply.github.com>
2020-10-17 14:16:54 -0400

    Add files via upload

---
+++ GitHub/Remdesivir/Cubes/scp.sh
@@ -0,0 +1,6 @@
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/remdesivir/HOMO_312_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/remdesivir/final/Cubes/
+
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/remdesivir/SOMO_313_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/remdesivir/final/Cubes/
+
+scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/remdesivir/LUMO_314_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/remdesivir/final/Cubes/
+
