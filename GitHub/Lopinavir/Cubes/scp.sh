df5e60ff0be0fa5caf4cbe703bbaf3104d2546fb
Jatin Kashyap <44009818+jatin19k@users.noreply.github.com>
2022-03-31 21:26:13 -0400

    Delete GitHub directory

--- GitHub/Lopinavir/Cubes/scp.sh
+++
@@ -1,4 +0,0 @@
-scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/lopinavir/HOMO_431_gamma.cube /Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/lopinavir/final/Cubes 
-
-scp jk435@lochness.njit.edu:/research/ddlab/jk435/MD_LAMMPS_ddlab/TEST/covid19/xsede_grant/DFT/fix_atoms/new_matlab/lopinavir/LUMO_432_gamma.cube
-/Users/kashyap/google_drive/NJIT/collaborations/COVID19/grants/xsede/submited/rebuttal/simulations/DFT/lopinavir/final/Cubes
